7f17f76315fff2b1d8f19389b1bd410d35ed32d4
Paulo de Tasso Oliveira de Lacerda <paulodetasso@proton.me>
2023-02-10 00:25:19 -0300

    feature: add await in server js middleware

--- src/server.js
+++ src/server.js
@@ -1,14 +1,38 @@
 import http from 'node:http';
 
-const server = http.createServer((req, res) => {
+const users = []
+
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
 
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch (error) {
+    req.body = null
+  }
+
+
   if (method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res.setHeader('Content-Type', 'application/json')
+      .end(JSON.stringify(users))
   }
 
   if (method === 'POST' && url === '/users') {
-    return res.end('Criação de usuário')
+    const { name, email } = req.body
+
+    users.push({
+      id: users.length + 1,
+      name,
+      email,
+    })
+
+    return res.writeHead(201).end()
   }
 
   return res.end('Hello World')
